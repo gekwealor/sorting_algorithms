4b4e93116547ee0e8d395316ddb53b7522b414d8
gekwealor <gekwealor@gmail.com>
2023-08-28 08:18:54 -0400

    Mr Bubbles

---
+++ print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_array - a funct that prints an array of ints
+ *
+ * @array: Printed arrays
+ * @size: Elements in a number of arrays 
+ */
+void print_array(const int *array, size_t size)
+{
+
+	size_t i;
+
+	i = 0;
+	while (array && i < size)
+	{
+
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
+}
